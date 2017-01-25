c195e264a567ce3b6b7ebb0d2646302f97125e51
s-matyukevich <s.matyukevich@gmail.com>
2017-01-25 14:56:04 -0700

    vault is used to store creds

--- ci/tasks/generate-manifest.sh
+++ ci/tasks/generate-manifest.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
-cat > manifest/tmp.yml < $MANIFEST
+cat > tmp/tmp-manifest.yml < $MANIFEST
 
-bosh interpolate manifest/tmp.yml -o concourse-deploy-mysql/ci/opfiles/common.yml > manifest/deployment.yml
+props=$(vault read -field=bosh-variables secret/mysql-props || true)
+cat $props > /tmp/props.yml
+
+bosh interpolate tmp/tmp-manifest.yml \
+  -o concourse-deploy-mysql/ci/opfiles/common.yml \
+  -v arbitrator-ip=$ARBITRATOR_IP
+  -v scdc1-master-ip=$SCDC1_MASTER_IP
+  -v scdc1-proxy-ip=$SCDC1_PROXY_IP
+  -v wdc1-master-ip=$WDC1_MASTER_IP
+  -v wdc1-proxy-ip=$WDC1_PROXY_IP
+  --vars-store /tmp/props.yml   > manifest/deployment.yml
+  
+vault write secret/mysql-props bosh-variables=@/tmp/props.yml
