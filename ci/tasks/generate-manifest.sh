5b2c9136ad52c5bcee1c388eb69ecc84de9e5840
s-matyukevich <s.matyukevich@gmail.com>
2017-01-25 15:56:16 -0700

    generate manifest script cahnged

--- ci/tasks/generate-manifest.sh
+++ ci/tasks/generate-manifest.sh
@@ -7,11 +7,11 @@ cat $props > /tmp/props.yml
 
 bosh interpolate /tmp/tmp-manifest.yml \
   -o concourse-deploy-mysql/ci/opfiles/common.yml \
-  -v arbitrator-ip=$ARBITRATOR_IP
-  -v scdc1-master-ip=$SCDC1_MASTER_IP
-  -v scdc1-proxy-ip=$SCDC1_PROXY_IP
-  -v wdc1-master-ip=$WDC1_MASTER_IP
-  -v wdc1-proxy-ip=$WDC1_PROXY_IP
+  -v arbitrator-ip=$ARBITRATOR_IP \
+  -v scdc1-master-ip=$SCDC1_MASTER_IP \
+  -v scdc1-proxy-ip=$SCDC1_PROXY_IP \
+  -v wdc1-master-ip=$WDC1_MASTER_IP \
+  -v wdc1-proxy-ip=$WDC1_PROXY_IP \
   --vars-store /tmp/props.yml   > manifest/deployment.yml
   
 vault write secret/mysql-props bosh-variables=@/tmp/props.yml
