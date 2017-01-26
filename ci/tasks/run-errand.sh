1ceaa79e365134e3ca1fde9469b7f63a1a3567fa
s-matyukevich <s.matyukevich@gmail.com>
2017-01-25 17:08:40 -0700

    bosh params fixed

--- ci/tasks/run-errand.sh
+++ ci/tasks/run-errand.sh
@@ -5,5 +5,5 @@ bosh_client=$(vault read -field=bosh-client-id secret/$VAULT_PROPERTIES_PATH)
 bosh_secret=$(vault read -field=bosh-secret secret/$VAULT_PROPERTIES_PATH)
 bosh_url=$(vault read -field=bosh-url secret/$VAULT_PROPERTIES_PATH)
 
-bosh -n -d mysql -e $bosh_url --ca-cert ca --client $bosh_client --client-secret $bosh_client_secret run-errand $BOSH_ERRAND
+bosh -n -d mysql -e $bosh_url --ca-cert ca --client $bosh_client --client-secret $bosh_secret run-errand $BOSH_ERRAND
 
