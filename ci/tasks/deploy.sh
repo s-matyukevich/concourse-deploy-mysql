26acf7b5f7ac36e7b452ed5d3112f891a4b8468b
s-matyukevich <s.matyukevich@gmail.com>
2017-02-03 13:46:56 -0700

    node count is now calculated

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -17,7 +17,9 @@ scdc_ips=${SCDC1_MASTER_IPS#"["}
 scdc_ips=${scdc_ips%"]"}
 wdc_ips=${WDC1_MASTER_IPS#"["}
 wdc_ips=${wdc_ips%"]"}
-cluster_ips="[$ARBITRATOR_IP,$scdc_ips,$wdc_ips]"
+cluster_ips="[$scdc_ips,$wdc_ips,$ARBITRATOR_IP]"
+IFS=',' read -ra scdc_ip_array <<< "$scdc_ips"
+IFS=',' read -ra wdc_ip_array <<< "$wdc_ips"
 
 bosh interpolate /tmp/tmp-manifest.yml \
   -o concourse-deploy-mysql/ci/opfiles/common.yml \
@@ -27,6 +29,8 @@ bosh interpolate /tmp/tmp-manifest.yml \
   -v scdc1-proxy-ip=$SCDC1_PROXY_IP \
   -v wdc1-master-ips=$WDC1_MASTER_IPS \
   -v wdc1-proxy-ip=$WDC1_PROXY_IP \
+  -v scdc1-master-nodes=${#scdc_ip_array[@]} \
+  -v wdc1-master-nodes=${#wdc_ip_array[@]} \
   --vars-store /tmp/props.yml   > deployment.yml
   
 vault write secret/mysql-props bosh-variables=@/tmp/props.yml
