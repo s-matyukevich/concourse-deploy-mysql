01219cb296a84b5048cddcfbd2971a8b6c2a69c1
s-matyukevich <s.matyukevich@gmail.com>
2017-02-08 12:06:18 -0700

    broker added

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -23,8 +23,10 @@ bosh interpolate /tmp/tmp-manifest.yml \
   -v backup-ip=$BACKUP_IP \
   -v scdc1-master-ips=$SCDC1_MASTER_IPS \
   -v scdc1-proxy-ip=$SCDC1_PROXY_IP \
+  -v scdc1-broker-ip=$SCDC1_BROKER_IP \
   -v wdc1-master-ips=$WDC1_MASTER_IPS \
   -v wdc1-proxy-ip=$WDC1_PROXY_IP \
+  -v wdc1-broker-ip=$WDC1_BROKER_IP \
   -v scdc1-master-nodes=${#scdc_ip_array[@]} \
   -v wdc1-master-nodes=${#wdc_ip_array[@]} \
   --vars-store /tmp/props.yml   > deployment.yml
