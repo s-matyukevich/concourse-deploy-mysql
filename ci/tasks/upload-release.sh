1d3ac3958bf275ac644eb46382d5a935fd1691dc
s-matyukevich <s.matyukevich@gmail.com>
2017-02-14 17:25:44 -0700

    deployment made configurable

--- ci/tasks/upload-release.sh
+++ ci/tasks/upload-release.sh
@@ -4,15 +4,15 @@ BASEDIR=$(dirname "$0")
 
 source $BASEDIR/common.sh  $CONTROL_PLANE_VAULT_PROPERTIES_PATH
 
-bosh -n upload-release $RELEASE_URL
-bosh -n upload-stemcell vsphere-stemcell/stemcell.tgz
+bosh -d cp-mysql -n upload-release $RELEASE_URL
+bosh -d cp-mysql -n upload-stemcell vsphere-stemcell/stemcell.tgz
 
 source $BASEDIR/common.sh  $SCDC1_VAULT_PROPERTIES_PATH
 
-bosh -n upload-release $RELEASE_URL
-bosh -n upload-stemcell vsphere-stemcell/stemcell.tgz
+bosh -d scdc1-mysql -n upload-release $RELEASE_URL
+bosh -d scdc1-mysql -n upload-stemcell vsphere-stemcell/stemcell.tgz
 
 source $BASEDIR/common.sh  $WDC1_VAULT_PROPERTIES_PATH
 
-bosh -n upload-release $RELEASE_URL
-bosh -n upload-stemcell vsphere-stemcell/stemcell.tgz
+bosh -d wdc1-mysql -n upload-release $RELEASE_URL
+bosh -d wdc1-mysql -n upload-stemcell vsphere-stemcell/stemcell.tgz
