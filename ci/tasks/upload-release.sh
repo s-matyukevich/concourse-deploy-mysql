0e84401bd7735ea1492b81994b783b2d8891a80a
s-matyukevich <s.matyukevich@gmail.com>
2017-02-16 18:06:01 -0700

    order of jobs changed

--- ci/tasks/upload-release.sh
+++ ci/tasks/upload-release.sh
@@ -7,12 +7,13 @@ source $BASEDIR/common.sh  $CONTROL_PLANE_VAULT_PROPERTIES_PATH
 bosh -d cp-mysql -n upload-release $RELEASE_URL
 bosh -d cp-mysql -n upload-stemcell vsphere-stemcell/stemcell.tgz
 
+source $BASEDIR/common.sh  $WDC1_VAULT_PROPERTIES_PATH
+
+bosh -d wdc1-mysql -n upload-release $RELEASE_URL
+bosh -d wdc1-mysql -n upload-stemcell vsphere-stemcell/stemcell.tgz
+
 source $BASEDIR/common.sh  $SCDC1_VAULT_PROPERTIES_PATH
 
 bosh -d scdc1-mysql -n upload-release $RELEASE_URL
 bosh -d scdc1-mysql -n upload-stemcell vsphere-stemcell/stemcell.tgz
 
-source $BASEDIR/common.sh  $WDC1_VAULT_PROPERTIES_PATH
-
-bosh -d wdc1-mysql -n upload-release $RELEASE_URL
-bosh -d wdc1-mysql -n upload-stemcell vsphere-stemcell/stemcell.tgz
