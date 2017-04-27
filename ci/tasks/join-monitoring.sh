d107d8376092b5190ae4409e1b3b7f361776deed
s-matyukevich <s.matyukevich@gmail.com>
2017-04-27 16:34:39 -0700

    joing monitoring path changed

--- ci/tasks/join-monitoring.sh
+++ ci/tasks/join-monitoring.sh
@@ -3,4 +3,4 @@
 BASEDIR=$(dirname "$0")
 source $BASEDIR/common.sh  $VAULT_PROPERTIES_PATH
 
-bosh vms | grep mysql | awk '{print $1}' | xargs -I id bosh ssh id 'sudo var/vcap/jobs/mysql/bin/join-monitoring'
+bosh vms | grep mysql | awk '{print $1}' | xargs -I id bosh ssh id 'sudo /var/vcap/jobs/mysql/bin/join-monitoring'
