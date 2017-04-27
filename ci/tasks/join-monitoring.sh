a58149314c0d43fbd3c01cb787f99f12563ca161
s-matyukevich <s.matyukevich@gmail.com>
2017-04-27 14:05:35 -0700

    join monitoring added

---
+++ ci/tasks/join-monitoring.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+BASEDIR=$(dirname "$0")
+source $BASEDIR/common.sh  $VAULT_PROPERTIES_PATH
+
+bosh vms | grep mysql | awk '{print $1}' | xargs -I id bosh ssh id 'sudo var/vcap/jobs/mysql/bin/join-monitoring'
